377209f44ff54c8b2a5d3bb72007749f99329130
miguelrm95 <miguelj.rodriguezmartinez@gmail.com>
2023-08-22 20:03:36 +0200

    feat: getAlbums rest client for json placeholder implemented

--- src/main/kotlin/org/migueljrm95/albumsapi/album/infrastructure/adapters/output/rest/AlbumsRestClient.kt
+++ src/main/kotlin/org/migueljrm95/albumsapi/album/infrastructure/adapters/output/rest/AlbumsRestClient.kt
@@ -2,7 +2,6 @@ package org.migueljrm95.albumsapi.album.infrastructure.adapters.output.rest
 
 import org.migueljrm95.albumsapi.album.domain.model.Album
 import org.springframework.cloud.openfeign.FeignClient
-import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Component
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
