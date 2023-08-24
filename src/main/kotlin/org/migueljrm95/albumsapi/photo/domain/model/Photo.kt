bd3189f33d09bef5eb2ba92d58343d2fc0f902e9
miguelrm95 <miguelj.rodriguezmartinez@gmail.com>
2023-08-24 09:01:44 +0200

    fix: photo model albumId type

--- src/main/kotlin/org/migueljrm95/albumsapi/photo/domain/model/Photo.kt
+++ src/main/kotlin/org/migueljrm95/albumsapi/photo/domain/model/Photo.kt
@@ -2,7 +2,7 @@ package org.migueljrm95.albumsapi.photo.domain.model
 
 class Photo(
     val id: Long,
-    val albumId: String,
+    val albumId: Long,
     val title: String,
     val url: String,
     val thumbnailUrl: String,
