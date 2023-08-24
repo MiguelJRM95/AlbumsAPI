b12fed1278b1b4e84ca81cebd2c3814d05b79346
miguelrm95 <miguelj.rodriguezmartinez@gmail.com>
2023-08-24 09:14:47 +0200

    test: album service refactor

--- src/test/kotlin/org/migueljrm95/albumsapi/album/domain/service/AlbumsServiceTest.kt
+++ src/test/kotlin/org/migueljrm95/albumsapi/album/domain/service/AlbumsServiceTest.kt
@@ -6,10 +6,14 @@ import org.junit.jupiter.api.extension.ExtendWith
 import org.migueljrm95.albumsapi.album.application.ports.output.AlbumsPort
 import org.migueljrm95.albumsapi.album.domain.model.Album
 import org.migueljrm95.albumsapi.photo.application.ports.output.PhotosPort
+import org.migueljrm95.albumsapi.photo.domain.model.Photo
+import org.mockito.ArgumentMatchers
 import org.mockito.InjectMocks
 import org.mockito.Mock
 import org.mockito.Mockito
 import org.mockito.junit.jupiter.MockitoExtension
+import org.mockito.kotlin.times
+import org.mockito.kotlin.verify
 
 @ExtendWith(MockitoExtension::class)
 class AlbumsServiceTest {
@@ -25,18 +29,21 @@ class AlbumsServiceTest {
 
     @Test
     fun getAlbums() {
-        Mockito.`when`(albumsPort.getAlbums(page)).thenReturn(albums)
-        val fetchedAlbums = albumsService.getAlbums(page)
+        Mockito.`when`(albumsPort.getAlbums(ArgumentMatchers.anyInt())).thenReturn(albums)
+        val fetchedAlbums = albumsService.getAlbums(ArgumentMatchers.anyInt())
+        verify(photosPort, times(3)).getPhotos(ArgumentMatchers.anyLong())
         assertNotNull(fetchedAlbums)
     }
 
     private companion object{
+        private val photoAlbum1 = Photo(1, 1, "Photo 1", "xxxx", "xxxx")
+        private val photoAlbum2 = Photo(2, 2, "Photo 2", "xxxx", "xxxx")
+        private val photoAlbum3 = Photo(3, 3, "Photo 3", "xxxx", "xxxx")
         private val albums =
             listOf(
-                Album(1, "Album 1", null),
-                Album(2, "Album 2", null),
-                Album(3, "Album 3", null)
+                Album(1, "Album 1", listOf(photoAlbum1)),
+                Album(2, "Album 2", listOf(photoAlbum2)),
+                Album(3, "Album 3", listOf(photoAlbum3))
             )
-        private val page = 1
     }
 }
